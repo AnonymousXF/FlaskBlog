486d076dfa997443a0e1484a55f3c950ed96c7c2
AnonymousXF <464059291@qq.com>
2018-09-13 20:16:13 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,5 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from config import *
 
+engine = create_engine(DevConfig.database_url, encoding='utf-8', echo=True)
