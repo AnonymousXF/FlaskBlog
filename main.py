c44b26808d413860baae200104154f191630f596
AnonymousXF <464059291@qq.com>
2018-09-16 11:40:16 +0800

    'md'

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# -*- coding: UTF-8 -*-
 from flask import Flask, render_template
 from config import *
 
 app = Flask(__name__)
-app.config.from_object(DevConfig)
+#app.config.from_object(DevConfig)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    file = open("D:\README.md", 'r', encoding="utf-8")
+    md = file.read()
+    file.close()
+    return render_template('index.html', markdown=md)
 
 if __name__ == "__main__":
     app.run()
