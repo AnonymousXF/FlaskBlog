a8c38fa33d5f49d03d4bd643668192cba85a7c0c
AnonymousXF <464059291@qq.com>
2018-09-17 21:15:01 +0800

    'md'

--- manage.py
+++ manage.py
@@ -3,4 +3,4 @@ from sqlalchemy.ext.declarative import declarative_base
 from config import *
 
 engine = create_engine(DevConfig.database_url, encoding='utf-8', echo=True)
-print(engine)
+#print(engine)
