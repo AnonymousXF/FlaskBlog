39edc696a8b27ee83edcd70e6d679e1da448defd
AnonymousXF <464059291@qq.com>
2018-09-25 17:10:33 +0800

    Update views.py

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,15 +1,28 @@
 #_*_coding:utf-8_*_
-from flask import render_template
+from flask import render_template, request
+from sqlalchemy import desc
 
 from . import blog
 from .. import db_session
 from ..models import Tag, Article
 
-@blog.route('/', methods=['GET','POST'])
+@blog.route('/', methods=['GET'])
 def bloglist():
     tags = db_session.query(Tag).all()
-    articles = db_session.query(Article.id, Article.title, Article.tag, Article.post_time).order_by(Article.post_time)
+    articles = db_session.query(Article.id, Article.title, Article.tag, Article.post_time).order_by(desc(Article.post_time))
     return render_template("bloglist.html", tags=tags, articles=articles)
+
+@blog.route('/list_by_tag', methods=['POST'])
+def list_by_tag():
+    tag = request.json
+    #print(tag)
+    tag_name = tag['tag']
+    if tag_name == "全部":
+        articles = db_session.query(Article.id, Article.title, Article.tag, Article.post_time).order_by(desc(Article.post_time))
+    else:
+        articles = db_session.query(Article.id, Article.title, Article.tag, Article.post_time).filter(Article.tag.like('%' + tag_name + '%')).order_by(desc(Article.post_time))
+    return render_template("list_by_tag.html", articles=articles, tag_name=tag_name)
+
 
 @blog.route('/article_detail/<article_id>')
 def article_detail(article_id):
